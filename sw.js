64022ed8cc0350d804801f9c2bcb4962a6e8b74d
pairoat sdin <sdin1971@gmail.com>
2026-08-19 15:54:02 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'midi-controller-standalone-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
